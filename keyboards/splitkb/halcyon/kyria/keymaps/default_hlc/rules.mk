d459782cc65d96751261f76903480b12bee6fc35
sts1981 <209857148+sts1981@users.noreply.github.com>
2025-05-01 21:53:05 -0400

    Setting up rules.mk and config.h files

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/rules.mk
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/rules.mk
@@ -1,4 +1,15 @@
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+MAGIC_ENABLE = no
+
+BOOTMAGIC_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+DYNAMIC_TAPPING_TERM_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+LAYER_LOCK_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+TAP_DANCE_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
