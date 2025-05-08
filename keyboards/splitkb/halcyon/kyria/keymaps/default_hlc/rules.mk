9f2f715c8acff6bfb8d3ae09d0bf74a727f7f26c
sts1981 <209857148+sts1981@users.noreply.github.com>
2025-05-08 00:46:26 -0400

    Adding mouse layer. Currently experimenting with Kinetic mode.

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/rules.mk
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/rules.mk
@@ -8,7 +8,7 @@ CAPS_WORD_ENABLE = yes
 #DYNAMIC_TAPPING_TERM_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 #LAYER_LOCK_ENABLE = yes
-#MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
