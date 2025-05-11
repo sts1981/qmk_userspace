8ed689c446513e989331f43ef428d9584251f0e2
sts1981 <209857148+sts1981@users.noreply.github.com>
2025-05-11 11:14:25 -0400

    Increasing leader timeout

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/config.h
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/config.h
@@ -13,8 +13,8 @@
 #define COMBO_TERM 75
 
 #define LEADER_PER_KEY_TIMING
-#define LEADER_NO_TIMEOUT // disable timeout for starting leader sequence
-#define LEADER_TIMEOUT 200
+//#define LEADER_NO_TIMEOUT // disable timeout for starting leader sequence
+#define LEADER_TIMEOUT 500
 
 //#define MK_COMBINED
 //#define MOUSEKEY_DELAY 0
