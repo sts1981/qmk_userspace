f9d5ea82038e3c7b47bbe7bd2d9ae5ea9f96768f
sts1981 <209857148+sts1981@users.noreply.github.com>
2025-06-14 20:02:59 -0400

    Disabling auto mouse layer when in trackpad scroll mode.

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
@@ -444,7 +444,6 @@ void leader_end_user(void) {
 static bool trackpad_scroll_mode = false;
 
 void pointing_device_init_user(void) {
-    // NOTE: auto-mouse layer is only activated when current layer is below mouse layer
     set_auto_mouse_layer(_MOUSE);
     set_auto_mouse_enable(true);
 }
@@ -480,11 +479,16 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
         case _NAV:
-            trackpad_scroll_mode = true;
-            // NOTE: Nav layer is above mouse layer, so don't need to toggle auto_mouse_enable
+            if (!trackpad_scroll_mode) {
+                trackpad_scroll_mode = true;
+                set_auto_mouse_enable(false);
+            }
             break;
         default:
-            trackpad_scroll_mode = false;
+            if (trackpad_scroll_mode) {
+                trackpad_scroll_mode = false;
+                set_auto_mouse_enable(true);
+            }
             break;
     }
     return state;
